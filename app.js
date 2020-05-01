49a2ce7715f21d18f9478954acffbc82e0e8692e
Sarah Carter <scarter3@conncoll.edu>
2020-05-01 12:30:42 -0400

    This commit will add the getRandomPhraseAsArray function, which randomly chooses and returns a phrase from an array

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const phrase = document.getElementById('phrase');
 const startButton = document.getElementById('startButton');
 let missed = 0;
 
-const phrases = ['black cat', 'cool beans', 'as if', 'groovy man', 'wishful thinking'];
+// Array of phrases
+const phrases = ['cool beans', 'as if', 'groovy man', 'black cat', 'wishful thinking'];
 
+// Listen for the start game button to be pressed
 startButton.addEventListener('click', () => {
     const overlay = document.getElementById('overlay');
     overlay.style.display = 'none';
 });
+
+// Randomly chooses and returns a phrase from the array
+const getRandomPhraseAsArray = arr => {
+    const getNumber = Math.floor((Math.random() * 5));
+    const arrayIndex = arr[getNumber];
+    return arrayIndex;
+}
+
+console.log(getRandomPhraseAsArray(phrases));
 
